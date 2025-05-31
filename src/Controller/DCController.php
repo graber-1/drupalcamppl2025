60a278f9ca6c558e64d62752020d36f50804b502
Marcin Grabias <mgrabias@ogtrading.eu>
2025-05-31 20:42:44 +0200

    Initial commit

---
+++ src/Controller/DCController.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\drupalcamppl_2025\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+
+/**
+ * DC Demo Controller.
+ */
+final class DCController extends ControllerBase {
+
+  /**
+   * Easy mode.
+   */
+  public function easy(): array {
+    $results = $this->entityTypeManager()->getStorage('node')->getQuery()
+      ->accessCheck(TRUE)
+      ->range(0, 1)
+      ->sort('nid', 'DESC')
+      ->execute();
+    if (\count($results) === 0) {
+      return [
+        '#markup' => $this->t('There are no nodes.'),
+      ];
+    }
+    return [
+      '#type' => 'link',
+      '#title' => $this->t('Display last node.'),
+      '#url' => Url::fromRoute('entity.node.canonical', [
+        'node' => \reset($results),
+      ]),
+      '#ajax' => [
+        'dialogType' => 'modal',
+        'dialog' => ['height' => 'auto', 'width' => '80%'],
+      ],
+    ];
+  }
+
+  /**
+   * Medium mode.
+   */
+  public function medium(): array {
+    $renderable = [];
+
+    $renderable['content'] = [
+      'container_1' => [
+        '#type' => 'container',
+        '#attributes' => ['id' => 'container-1'],
+        'content' => [
+          '#markup' => $this->t('Content of Container 1'),
+        ],
+      ],
+      'container_2' => [
+        '#type' => 'container',
+        '#attributes' => ['id' => 'container-2'],
+        'content' => [
+          '#markup' => $this->t('Content of Container 2'),
+        ],
+      ],
+    ];
+
+    $renderable['link'] = [
+      '#type' => 'link',
+      '#title' => $this->t('Open modal form.'),
+      '#url' => Url::fromRoute('drupalcamppl_2025.medium.form'),
+      '#ajax' => [
+        'dialogType' => 'modal',
+        'dialog' => ['height' => 'auto', 'width' => '80%'],
+      ],
+    ];
+
+    return $renderable;
+  }
+
+}
---
+++ src/Form/MediumForm.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\drupalcamppl_2025\Form;
+
+use Drupal\Core\Ajax\AjaxFormHelperTrait;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\CloseModalDialogCommand;
+use Drupal\Core\Ajax\RemoveCommand;
+use Drupal\Core\Ajax\ReplaceCommand;
+use Drupal\Core\Ajax\AppendCommand;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\Html;
+
+/**
+ * Medium difficulty form.
+ */
+final class MediumForm extends FormBase {
+  use AjaxFormHelperTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'dc_2025_medium_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form_state->setRebuild();
+
+    $form['messages'] = [
+      '#theme' => 'status_messages',
+      '#status_headings' => [
+        'error' => $this->t('Error message'),
+        'warning' => $this->t('Warning message'),
+      ],
+      '#message_list' => [],
+      '#weight' => -100,
+    ];
+
+    $form['command'] = [
+      '#title' => $this->t('Command'),
+      '#type' => 'radios',
+      '#required' => TRUE,
+      '#options' => [
+        'replace' => $this->t('Replace some content'),
+        'remove' => $this->t('Remove some content'),
+        'append' => $this->t('Append some content'),
+      ],
+    ];
+
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+      '#ajax' => [
+        'callback' => [$this, 'ajaxSubmit'],
+      ],
+    ];
+
+    // Core issue: https://www.drupal.org/node/2897377.
+    $form['#id'] = Html::getId($form_state->getBuildInfo()['form_id']);
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Not needed for demonstration purposes.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function successfulAjaxSubmit(array $form, FormStateInterface $form_state): AjaxResponse {
+    $response = new AjaxResponse();
+
+    $command = $form_state->getValue('command');
+
+    if ($command === 'replace') {
+      $response->addCommand(new ReplaceCommand('#container-1', [
+        '#type' => 'container',
+        '#attributes' => ['id' => 'container-1'],
+        'content' => [
+          '#markup' => $this->t('Replaced content of Container 1'),
+        ],
+      ]));
+    }
+    elseif ($command === 'remove') {
+      $response->addCommand(new RemoveCommand('#container-2'));
+    }
+    elseif ($command === 'append') {
+      $response->addCommand(new AppendCommand('#container-1', [
+        '#type' => 'container',
+        '#attributes' => ['id' => 'container-1-1'],
+        'content' => [
+          '#markup' => $this->t('Content appended to container 1'),
+        ],
+      ]));
+    }
+
+    $response->addCommand(new CloseModalDialogCommand());
+
+    return $response;
+  }
+
+}
